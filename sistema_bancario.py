2b5634afc4c420ff0ddc6a81c6ea69122011e05c
Caio Dias <93464556+caiorodias@users.noreply.github.com>
2023-02-09 13:02:41 -0300

    atualizando sis

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -19,47 +19,68 @@ while True:
     opcao = int(input(menu))
         
     if opcao == 1:
-        print('Depósito')
+        
+        print('-----------------DEPÓSITO-----------------')
         valor_deposito = int(input('Informe qual o valor do depósito em reais:  '))
+        
         if valor_deposito <= 0:
             print('Insira um valor válido')
+            
         else:
+            
             saldo += valor_deposito
+            
             print(f'''
 Depósito realizado com sucesso!
 Seu saldo atual é R$ {saldo:.2f}
                   ''')
+            
             """ adicionar ao extrato """
             operacoes.append({'Tipo': 'Depósito', 'Valor': valor_deposito})
         
     elif opcao == 2:
+        
         if numero_saques >= LIMITE_SAQUES:
+            
             print('Você já realizou a quantidade limite de saques diário.')
             continue
+        
         else:
-            print('Saque')
+            
+            print('-----------------SAQUE-----------------')
             print(f'Seu saldo atual é {saldo}')
             valor_saque = int(input('Informe qual valor deseja sacar:  '))
         
         if valor_saque > saldo:
+            
             print('Saldo insuficiente.')
+            
         else:
+            
             if valor_saque > limite:
                 print('Valor que deseja sacar ultrapassa limite diário.')
+                
             else:
+                
                 saldo -= valor_saque                    
                 print('Saque realizado, retire o dinheiro na boca do caixa.')
                 print(f'Saldo atual {saldo}')
                 numero_saques += 1
                 """ adicionar ao extrato """
                 operacoes.append({'Tipo': 'Saque', 'Valor': valor_saque})
+                
+                
     elif opcao == 3:
-        print('Extrato')
+        
+        print('-----------------EXTRATO-----------------')
         for i in operacoes:
             print(i)
+        print(f'Seu saldo atual é {saldo:.2f}.')
         
     elif opcao == 0:
+        
         break
     
     else:
+        
         print('Opção inválida!')
